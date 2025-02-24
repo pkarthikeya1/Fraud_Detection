a1ad434d58b02aa756d88ad90f705e0784af507a
karthikeya pervela <69100096+pkarthikeya1@users.noreply.github.com>
2025-02-24 19:26:19 +0530

    added api

--- src/frontend.py
+++ src/frontend.py
@@ -72,4 +72,4 @@ def predict_file(file: UploadFile = File(...)):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
